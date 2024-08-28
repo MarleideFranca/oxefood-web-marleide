67db8bdd60a818e3a6fdfff0285728d4f63aeb9f
Marleide França <marleidealves2002@yahoo.com.br>
2024-08-28 14:57:28 -0300

    Implementando a exibição de mensagens no projeto

--- src/views/cliente/FormCliente.jsx
+++ src/views/cliente/FormCliente.jsx
@@ -4,6 +4,7 @@ import { Link, useLocation } from "react-router-dom";
 import InputMask from "react-input-mask";
 import { Button, Container, Divider, Form, Icon } from "semantic-ui-react";
 import MenuSistema from "../../MenuSistema";
+import { notifyError, notifySuccess } from "../../views/util/Util";
 
 export default function FormCliente() {
   const { state } = useLocation();
@@ -44,20 +45,38 @@ export default function FormCliente() {
       axios
         .put("http://localhost:8080/api/cliente/" + idCliente, clienteRequest)
         .then((response) => {
-          console.log("Cliente alterado com sucesso.");
+          // console.log("Cliente alterado com sucesso.");
+          notifySuccess = ("Cliente alterado com sucesso.")
         })
         .catch((error) => {
-          console.log("Erro ao alterar um cliente.");
+          // console.log("Erro ao alterar um cliente.");
+          if (error.response.data.errors != undefined) {
+            for (let i = 0; i < error.response.data.errors.length; i++) {
+              notifyError(error.response.data.errors[i].defaultMessage)
+            }
+          } else {
+            notifyError(error.response.data.message)
+          }
+
         });
     } else {
       //Cadastro:
       axios
         .post("http://localhost:8080/api/cliente", clienteRequest)
         .then((response) => {
-          console.log("Cliente cadastrado com sucesso.");
+          //console.log("Cliente cadastrado com sucesso.");
+          notifySuccess = ("Cliente cadastrado com sucesso.")
         })
         .catch((error) => {
-          console.log("Erro ao incluir o cliente.");
+          //console.log("Erro ao incluir o cliente.");
+          if (error.response.data.errors != undefined) {
+            for (let i = 0; i < error.response.data.errors.length; i++) {
+              notifyError(error.response.data.errors[i].defaultMessage)
+            }
+          } else {
+            notifyError(error.response.data.message)
+          }
+
         });
     }
   }
